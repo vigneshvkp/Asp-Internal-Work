ebad1253e08b02d4b7a6d6ba6a8900e81333bcdb
vigneshvkp <vigneshvkp@gmail.com>
2017-09-15 15:36:22 +0530

    tem

--- weitage.sql
+++ weitage.sql
@@ -20,33 +20,34 @@ USE `thi_prod_bug`;
 -- Dumping structure for table thi_prod_bug.weitage
 DROP TABLE IF EXISTS `weitage`;
 CREATE TABLE IF NOT EXISTS `weitage` (
+  `id` int(10) unsigned NOT NULL,
   `line_item` varchar(100) DEFAULT NULL,
-  `percentage` float DEFAULT NULL
+  `percentage` integer DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table thi_prod_bug.weitage: ~20 rows (approximately)
 /*!40000 ALTER TABLE `weitage` DISABLE KEYS */;
-INSERT INTO `weitage` (`line_item`, `percentage`) VALUES
-	('Development Environment', 0.35),
-	('QA environment', 0.25),
-	('UAT Environment', 0.1),
-	('Code Synchronization with target repository', 0.3),
-	('Technical and Non functional Requirements', 0.35),
-	('Requirement Analysis', 0.3),
-	('Impact Analysis', 0.35),
-	('Technical Design (HLD or LLD)', 0.5),
-	('Database Design', 0.5),
-	('Server Code Quality\r\n', 0.2),
-	('Client Code Quality\r\n', 0.2),
-	('DB Scripts Quality\r\n', 0.2),
-	('Automated Code Analysis (Server & Client)\r\n', 0.2),
-	('Design Adherence\r\n', 0.2),
-	('Automated Unit test classes\r\n', 0.4),
-	('Functional Unit test case for the implemented feat\r\n', 0.6),
-	('Build procedure\r\n', 0.25),
-	('Deployment procedure for all environments\r\n', 0.25),
-	('Code Release & merge\r\n', 0.5),
-	('Recommended practices for project\r\n', 1);
+INSERT INTO `weitage` (`id`,`line_item`, `percentage`) VALUES
+	(1,'Development Environment', 35),
+	(2,'QA environment', 25),
+	(3,'UAT Environment', 10),
+	(4,'Code Synchronization with target repository', 30),
+	(5,'Technical and Non functional Requirements', 35),
+	(6,'Requirement Analysis', 30),
+	(7,'Impact Analysis', 35),
+	(8,'Technical Design (HLD or LLD)', 50),
+	(9,'Database Design', 50),
+	(10,'Server Code Quality\r\n', 20),
+	(11,'Client Code Quality\r\n', 20),
+	(12,'DB Scripts Quality\r\n', 20),
+	(13,'Automated Code Analysis (Server & Client)\r\n', 20),
+	(14,'Design Adherence\r\n', 20),
+	(15,'Automated Unit test classes\r\n', 40),
+	(16,'Functional Unit test case for the implemented feat\r\n', 60),
+	(17,'Build procedure\r\n', 25),
+	(18,'Deployment procedure for all environments\r\n', 25),
+	(19,'Code Release & merge\r\n', 50),
+	(20,'Recommended practices for project\r\n', 10);
 /*!40000 ALTER TABLE `weitage` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
