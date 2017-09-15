b5a82c9f59d24b5def521dc97511c26137b4d826
vigneshvkp <vigneshvkp@gmail.com>
2017-09-15 17:39:30 +0530

    new class

---
+++ THI_Production/src/com/aspire/thi/domain/LineItemScore.java
@@ -0,0 +1,25 @@
+package com.aspire.thi.domain;
+
+public class LineItemScore {
+
+	private String lineItem;
+	private int percentage;
+
+	public String getLineItem() {
+		return lineItem;
+	}
+
+	public void setLineItem(String lineItem) {
+		this.lineItem = lineItem;
+	}
+
+	public int getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(int percentage) {
+		this.percentage = percentage;
+	}
+
+	
+}
