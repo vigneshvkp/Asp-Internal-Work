fb1364bbd5cc9165fa30902a127ee08222c32437
vigneshvkp <vigneshvkp@gmail.com>
2017-09-15 15:11:25 +0530

    weitage.sql

--- weitage.sql
+++ weitage.sql
@@ -20,13 +20,13 @@ USE `thi_prod_bug`;
 -- Dumping structure for table thi_prod_bug.weitage
 DROP TABLE IF EXISTS `weitage`;
 CREATE TABLE IF NOT EXISTS `weitage` (
-  `Name` varchar(100) DEFAULT NULL,
-  `Percentage` float DEFAULT NULL
+  `line_item` varchar(100) DEFAULT NULL,
+  `percentage` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table thi_prod_bug.weitage: ~0 rows (approximately)
+-- Dumping data for table thi_prod_bug.weitage: ~20 rows (approximately)
 /*!40000 ALTER TABLE `weitage` DISABLE KEYS */;
-INSERT INTO `weitage` (`Name`, `Percentage`) VALUES
+INSERT INTO `weitage` (`line_item`, `percentage`) VALUES
 	('Development Environment', 0.35),
 	('QA environment', 0.25),
 	('UAT Environment', 0.1),
